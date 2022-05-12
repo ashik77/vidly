4caa14d081b7a65eb2bbe72195e2e298e5d687f5
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-05-13 00:08:59 +0300

    handling multiple inputs using controlled element

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
-  username = React.createRef();
+  state = {
+    account: { username: "", password: "" },
+  };
 
   handleSubmit = (e) => {
     e.preventDefault();
@@ -9,7 +11,15 @@ class LoginForm extends Component {
     //call the server
     //redirect the user to a different page
   };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -18,15 +28,24 @@ class LoginForm extends Component {
             <label htmlFor="username">Username</label>
             <input
               autoFocus
-              ref={this.username}
+              value={account.username}
+              onChange={this.handleChange}
               id="username"
+              name="username"
               type="text"
               className="form-control"
             />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              name="password"
+              id="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
