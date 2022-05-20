88a9777f6c74335f8ac68a9ad5174371bdbe5868
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-05-20 14:08:16 +0300

    saving and updating the movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -11,7 +11,16 @@ export function getMovie(movieId) {
   return http.get(apiEndpoint + "/" + movieId);
 }
 
-export function saveMovie(movie) {}
+export function saveMovie(movie) {
+  //updating movie
+  if (movie._id) {
+    const body = { ...movie };
+    delete body._id; //since movie obj has id property and our restful api in the backend doesn't like the id in the body property, so one id in url and one in body of the request which is confusing. so we should remove the id from movie obj but also our movie obj is part of the state so we should not directly modify our state.
+    return http.put(apiEndpoint + "/" + movie._id, body);
+  }
+  //saving a new movie
+  return http.post(apiEndpoint, movie);
+}
 
 export function deleteMovie(movieId) {
   return http.delete(apiEndpoint + "/" + movieId);
