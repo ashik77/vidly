c6c20b9e0300dcb64f615cbc08aff02cba63e281
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-05-20 14:17:04 +0300

    refactoring movieService

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,12 +3,16 @@ import config from "../config.json";
 
 const apiEndpoint = config.apiUrl + "/movies";
 
+function movieUrl(id) {
+  return `${apiEndpoint}/{id}`;
+}
+
 export function getMovies() {
   return http.get(apiEndpoint);
 }
 
 export function getMovie(movieId) {
-  return http.get(apiEndpoint + "/" + movieId);
+  return http.get(movieUrl(movieId));
 }
 
 export function saveMovie(movie) {
@@ -16,12 +20,12 @@ export function saveMovie(movie) {
   if (movie._id) {
     const body = { ...movie };
     delete body._id; //since movie obj has id property and our restful api in the backend doesn't like the id in the body property, so one id in url and one in body of the request which is confusing. so we should remove the id from movie obj but also our movie obj is part of the state so we should not directly modify our state.
-    return http.put(apiEndpoint + "/" + movie._id, body);
+    return http.put(movieUrl(movie._id), body);
   }
   //saving a new movie
   return http.post(apiEndpoint, movie);
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(apiEndpoint + "/" + movieId);
+  return http.delete(movieUrl(movieId));
 }
